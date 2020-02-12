52670218acd805b4830235f444b42eee60033330
Aaron Imbrock <aimbrock@gmail.com>
2020-02-11 16:45:47 -0800

    Chess board squares all render correctly

---
+++ chess_online/src/components/GamePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ChessBoard from "./ChessBoard";
+
+class GamePage extends React.Component {
+    render() {
+      return <ChessBoard />
+    }
+  }
+
+export default GamePage
