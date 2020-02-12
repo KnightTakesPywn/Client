4f055190a464e17a982d8828d4a3952eb2573742
Aaron Imbrock <aimbrock@gmail.com>
2020-02-12 12:22:07 -0800

    Chess pieces render on board

--- chess_online/src/components/ChessBoard.js
+++ chess_online/src/components/ChessBoard.js
@@ -2,18 +2,15 @@ import React, {Component} from "react";
 import "../../src/ChessBoard.css";
 
 class ChessBoard extends Component {
-  
   constructor(props) {
     super(props);
     const board = props.data.board
-    console.log("props:", board)
     this.state = {
       error: null,
       isLoaded: false,
       board: board,
     };
   }
-
 
   render() {
     // const { error, isLoaded, board } = this.state;
@@ -23,7 +20,6 @@ class ChessBoard extends Component {
       // } else if (!isLoaded) {
       //   return <div>Loading...</div>;
     } else {
-      console.log('Render', board)
       
       return (
         <div id="board">{board.map( (row, index) => renderRow(row, index))}</div>
@@ -33,34 +29,35 @@ class ChessBoard extends Component {
 }
 
 
+
   // export default function ChessBoard() {
     //   return <div className="App">{board.map(row => renderRow(row))}</div>;
     // }
     // { "name": "BR", "type": "rook", "color": "black", "pos_row": 0, "pos_col": 0, "image": "url" },
-  function renderRow(row, index) {
-    console.log('Row:', row);
-    let x_loc = index
+function renderRow(row, index) {
+  console.log('Row:', row);
+  let x_loc = index
     
-    return (<>
-    { row.map( (square, index) => {
-      let y_loc = index
-      let square_color = (y_loc + x_loc) % 2 === 0 ? 'white' : 'black'
-      if (square !== null) { 
-        return <Square
-        x_loc={x_loc}
-        y_loc={y_loc}
-        square_color={square_color}
-        chesspiece={square.type}
-        pieceColor={square.pieceColor}
-        />
-      } else {
-        return <Square 
-        x_loc={x_loc}
-        y_loc={y_loc}
-        square_color={square_color}
-        chesspiece=''
-        pieceColor=''
-        />
+  return (<>
+  { row.map( (square, index) => {
+    let y_loc = index
+    let square_color = (y_loc + x_loc) % 2 === 0 ? 'white' : 'black'
+    if (square !== null) { 
+      return <Square
+      x_loc={x_loc}
+      y_loc={y_loc}
+      square_color={square_color}
+      chesspiece={square.type}
+      pieceColor={square.color}
+      />
+    } else {
+      return <Square 
+      x_loc={x_loc}
+      y_loc={y_loc}
+      square_color={square_color}
+      chesspiece=''
+      pieceColor=''
+      />
       }
     })}
   </>)
@@ -72,9 +69,9 @@ function Square(props) {
   If piece then render Piece as well
   */
   return (
-    <div className={props.square_color + " square"} id={props.x_loc + props.y_loc}>
+    <div className={props.square_color + " square"} id={`${props.x_loc}${props.y_loc}`}>
      {/* DANGER: need to only render piece if piece exists */}
-      <div>Square: {props.x_loc},{props.y_loc}</div>
+      <div>{props.x_loc},{props.y_loc}</div>
       {props.chesspiece && (
         <Piece rank={props.chesspiece} color={props.pieceColor} />
       )}
@@ -83,12 +80,28 @@ function Square(props) {
 }
 
 function Piece(props) {
+  const chess_pieces = {
+    'white_king': '♔',
+    'white_queen': '♕',
+    'white_rook': '♖',
+    'white_bishop': '♗',
+    'white_knight': '♘',
+    'white_pawn': '♙',
+    'black_king': '♚',
+    'black_queen': '♛',
+    'black_rook': '♜',
+    'black_bishop': '♝',
+    'black_knight': '♞',
+    'black_pawn': '♟',
+  }
+
+  // {`${props.x_loc}${props.y_loc}`}
+  console.log(`props.color:' ${props.color}_${props.rank}`)
   return (
     <p>
-      {props.color} {props.rank}
+      {chess_pieces[`${props.color}_${props.rank}`]}
     </p>
   );
 }
-
 
 export default ChessBoard
